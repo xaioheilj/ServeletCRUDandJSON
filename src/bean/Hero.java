4119834708c03b88acb5871692927456ac162560
1419526625@qq.com <1419526625@qq.com>
2020-03-29 20:14:48 +0800

    servlet实现增删改查和加入JSON的使用

---
+++ src/bean/Hero.java
@@ -0,0 +1,61 @@
+package bean;
+
+public class Hero {
+    public int id;
+    public String name;
+    public float hp;
+    public int damage;
+
+    public Hero(int id, String name, float hp, int damage) {
+        this.id = id;
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+    }
+
+    public Hero() {
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getHp() {
+        return hp;
+    }
+
+    public void setHp(float hp) {
+        this.hp = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", hp=" + hp +
+                '}';
+    }
+}
---
+++ src/servlet/GetOneServlet.java
@@ -0,0 +1,28 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Hero;
+import net.sf.json.JSONObject;
+
+public class GetOneServlet extends HttpServlet {
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Hero hero = new Hero();
+        hero.setName("盖伦");
+        hero.setHp(353);
+
+        JSONObject json= new JSONObject();
+
+        json.put("hero", JSONObject.fromObject(hero));
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().print(json);
+    }
+} 
